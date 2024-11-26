14b0b19882dba92ad132f3e59956047aa0183799
alemawit <alemawit.woldu@gmail.com>
2024-11-25 21:45:51 -0500

    baseURL toggle of firebase function and deployed amazon server

--- src/Components/Api/axios.js
+++ src/Components/Api/axios.js
@@ -4,6 +4,9 @@ const axiosInstance = axios.create({
   //local instance of firebase functions
   //   baseURL: "http://127.0.0.1:5001/clone-3ed40/us-central1/api",
   //deployed version of amazon server on render.com
-  baseURL: "https://amazon-api-deploy-2g2k.onrender.com",
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? "https://amazon-api-deploy-2g2k.onrender.com"
+      : "http://127.0.0.1:5001/clone-3ed40/us-central1/api",
 });
 export {axiosInstance}
